7a0a00465cd07f2b8d171d68ac02a3ca029b66d2
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-29 22:49:56 -0700

    adding files again

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Main from './components/main';
 import Wrapper from './components/wrapper';
-import Header from './components/header';
-import './App.css';
+import Header from './components/Header';
+import './index.css';
 
 function App() {
-  return
+  return (
   <>
   <Wrapper>
     <Header />
     <Main /> 
   </Wrapper>
 </>
-  );
+);
 }
 
 export default App;
