4c0cd3133419770c97e9efe51e63cb2c0b5e3bf9
Kathleen Yoannon <k.yoannon@gmail.com>
2020-10-04 19:34:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(require("./src/utils/api"));
+
 const uri = "mongodb+srv://kjy_24:<Snow123Joy>@clusterkjy.lcvdc.mongodb.net/<re>?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
